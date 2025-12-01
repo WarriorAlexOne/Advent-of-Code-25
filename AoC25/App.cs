492bfd81390ae93d45170874ce6e9d8ba0fc2265
WarriorAlexOne <warrioralexone@gmail.com>
2025-12-01 05:04:40 -0600

    Advent of Code 2025 Start

---
+++ AoC25/App.cs
@@ -0,0 +1,21 @@
+﻿class App
+{
+    public static void Main (string[] args)
+    {
+        if (args.Length <= 0)
+        {
+            Console.WriteLine("No Args!!!");
+            return;
+        }
+        
+        switch (args[0])
+        {
+            default:
+                Console.WriteLine("Invalid Args!!!");
+                break;
+            case "1":
+                Day1.Day1_();
+                break;
+        }
+    }
+}
---
+++ AoC25/day1/Day1.cs
@@ -0,0 +1,7 @@
+﻿class Day1
+{
+    public static void Day1_ ()
+    {
+        Console.WriteLine("Am I Looping Day1?");
+    }
+}
